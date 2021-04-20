eecb6623afdb67a0638696549e18a250a3d65f5e
ohha-ryo08 <81632828+ohha-ryo08@users.noreply.github.com>
2021-04-20 12:08:36 +0900

    php第二章　foreach
    課題提出

---
+++ 2-8/text.php
@@ -0,0 +1,10 @@
+<?php
+$fruits = ["apple" => "といったらりんご","orange" => "といったらみかん","peach" => "といったらもも"];
+
+foreach ($fruits as $key => $value) {
+    echo $key;
+    echo $value;
+    echo '<br />';
+}
+
+?>
