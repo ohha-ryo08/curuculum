6902176d385993f72dee55129a715d1854a21b7d
ohha-ryo08 <81632828+ohha-ryo08@users.noreply.github.com>
2021-04-21 22:20:28 +0900

    php第二章　フォームの内容を取得する
    課題提出

---
+++ 2-11/result.php
@@ -0,0 +1,8 @@
+<?php
+$my_name = $_POST['my_name'];
+$mail_address = $_POST['mail_address'];
+$password = $_POST['password'];
+?>
+<p>私の名前は、<?php echo $my_name; ?></p>
+<p>私のメールアドレスは、<?php echo $mail_address; ?></p>
+<p>私のパスワードは、<?php echo $password; ?></p>
