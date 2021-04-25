3d2c678b1f1d9897bf6a8950f876abe2cff2a3d9
ohha-ryo08 <81632828+ohha-ryo08@users.noreply.github.com>
2021-04-25 18:10:02 +0900

    php第二章 いろんな入力フォーム
    課題提出

---
+++ 2-12/result.php
@@ -0,0 +1,10 @@
+<?php
+    $my_name = $_POST['my_name'];
+    $rank = $_POST['rank'];
+    $rank = $_POST['rank'];
+    $rank = $_POST['rank'];
+    $number = $_POST['number'];
+    ?>
+    <p>お名前:<?php echo $my_name; ?></p>
+    <p>ご希望景品<?php echo $rank; ?></p>
+    <p>個数:<?php echo $number; ?></p>
